dcdd15caa455c18cdc4ac6ff0966ffaa4b8b770c
Latifah President <latifahpresident@Latifahs-MacBook-Pro.local>
2019-01-14 13:19:10 -0600

    get by id working

--- server.js
+++ server.js
@@ -26,5 +26,19 @@ server.get(url, async(req, res) => {
     }
 });
 
+server.get(`${url}:id`, async(req, res) => {
+    const { id } = req.params;
+    try{
+        const projectsData = await projects.get(id)
+        if(projectsData.length === 0) {
+            res.status(404).json(`{error: 'that user can be found'}`)
+        } else {
+            res.status(200).json(projectsData)
+        }
+    } catch(err){
+        res.status(500).json(`{error: 'user info could not be found'}`)
+    }
+});
+
 module.exports = server
 
